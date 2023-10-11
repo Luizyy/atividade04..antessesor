634d48523b6f7c1307459cc89994ed51e219755e
Luizyy <analuizynascimentodasilva@gmail.com>
2023-10-11 10:57:04 -0300

    atividade 04 sucessor antecessor

---
+++ atividade04.py
@@ -0,0 +1,4 @@
+# Escreva um numero e veja qual o seu antecessor e seu sucessor! 
+numero = float(input("Digite o número para obter o sucessor e antecessor"))
+print ('o antecessor é' , numero + 1)
+print ('o antecessor é' , numero - 1)
